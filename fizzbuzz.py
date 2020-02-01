ed6b5644144bc6d1b8d26c34196029fde03f3906
dblancoformacion <dblancoformacion@gmail.com>
2020-02-01 13:08:30 +0100

    Requisitos 3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -7,12 +7,12 @@ NUMERO_BUZZ = 5
 class Kata:
 	def esDivisible(self,numero,divisor):
 		return True if numero%divisor==0 else False
-	def contiene(self):
-		return True
+	def contiene(self,pajar,aguja):
+		return True if str(pajar).find(str(aguja))>0 else False
 	def esFizz(self,numero):
-		return self.esDivisible(numero,NUMERO_FIZZ)
+		return self.esDivisible(numero,NUMERO_FIZZ) or self.contiene(numero,NUMERO_FIZZ)
 	def esBuzz(self,numero):
-		return self.esDivisible(numero,NUMERO_BUZZ)
+		return self.esDivisible(numero,NUMERO_BUZZ) or self.contiene(numero,NUMERO_BUZZ)
 	def esFizzBuzz(self,numero):
 		return self.esDivisible(numero,NUMERO_FIZZ) and self.esDivisible(numero,NUMERO_BUZZ)
 	def fizzbuzz(self,numero):
@@ -29,9 +29,12 @@ class Kata:
 if __name__ == "__main__":
 
 
+
 	kata=Kata()
+
 	for i in range(100):
 		print(i+1,kata.fizzbuzz(i+1))
+
 '''
 print(1,fizzbuzz(1)==1)
 print(3,fizzbuzz(3)=='Fizz')
