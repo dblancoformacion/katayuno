7cd667e411e105da90627ecc548ff8856ae51ad3
dblancoformacion <dblancoformacion@gmail.com>
2020-02-05 09:46:23 +0100

    Instrucciones

---
+++ instrucciones.php
@@ -0,0 +1,51 @@
+<?php
+if(1){
+/*
+FIZZBUZZ
+cada hora iteramos
+50 minutos para resolver el problema
+10 minutos de restrospectiva y planning
+Pair programming (por parejas)
+Usar TDD (Test driven development), o al menos test del código, de lo contrario no puedo demostrar que funciona. Si no hay test, no hay código
+*/
+/*
+Agnóstico del lenguaje
+No completar el problema
+Enfócate en la práctica
+Código abierto a cambios
+Seguir las 4 reglas del diseño simple
+Divertirse
+*/
+/*
+Dominio del problema: todo lo no técnico del problema, su contexto
+Romper viejos hábitos
+Realizar código de calidad
+Diseño modular
+Test del código
+*/
+/* Reglas del diseño simple
+[] fácil y barato
+[] los test pasan
+[] el código expresa la intención (entenderlo sin saber el lenguaje)
+[] dry (don't repeat yourself)
+[] método pequeño: cuando no pasa de las 5 líneas
+[] clase pequeña: cuando tiene entre 3 y 6 métodos
+[] principio solid: debe ocuparse de una sóla cosa
+*/
+/* TDD
+define el test
+programa lo mínimo para que pase
+
+*/
+/*
+cyber-dojo.org
+katayuno
+*/
+/* unit test
+pruebas unitarias que comprueban
+
+*/
+/* tdd
+primero haces las pruebas y en base a esas pruebas programamos
+*/
+}
